248e6a70423034bedb441c235b923ba5bd8b2637
MRizkyF27 <63577711+Rizky2705@users.noreply.github.com>
2021-10-08 09:05:36 +0700

    menyelesaikan video 3 Struktur Folder, Routes & view

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhamad Rizky Fauzan",
+        "email" => "m.rizky2705@gmail.com",
+        "image" => "profile.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
