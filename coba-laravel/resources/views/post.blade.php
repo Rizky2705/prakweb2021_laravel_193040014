8dacb84be069e7f7d38f8a1303032fa874bf9360
MRizkyF27 <63577711+Rizky2705@users.noreply.github.com>
2021-10-15 07:52:35 +0700

    Menyelesaikan viedo Post Category & Eloquent Relationship

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -1,11 +1,8 @@
 @extends('layouts.main')
-
 @section('container')
-<article>
-    <h2>{{ $post->title }}</h2>
-    <h5>{{ $post->author }}</h5>
-    {!! $post->body !!}
-</article>
+<h1 class="mb-5">{{ $post->title }}</h1>
+<p>By. Muhamad Rizky Fauzan in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+{!! $post->body !!}
 
 <a href="/blog">Back to Posts</a>
 @endsection
