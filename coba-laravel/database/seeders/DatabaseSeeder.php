391477a4322d22fc9950da12f291f95d1be947ac
MRizkyF27 <63577711+Rizky2705@users.noreply.github.com>
2021-10-25 21:04:41 +0700

    Menyelesaikan video Database Seeder

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,64 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Muhamad Rizky Fauzan',
+            'email' => 'm.rizky2705@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Sandhika Galih',
+            'email' => 'sandhikagalih@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-proframming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, laborum.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aspernatur impedit cupiditate perferendis nemo facere velit consequatur commodi, incidunt eveniet rerum doloribus tenetur magnam repellat voluptatibus hic provident vero earum omnis magni in. Repellat ratione praesentium sunt, nihil harum ipsum aut aspernatur similique, a molestias explicabo quam. Totam sapiente sed provident neque itaque veritatis nulla quaerat corrupti obcaecati quos? Possimus accusamus, iste illo et, repellat quod quia facere, voluptatibus totam vero laborum nostrum consequatur? Hic debitis natus officiis. Optio esse dolor nobis neque veritatis, repellendus reiciendis porro tenetur? Consequuntur eveniet explicabo est autem nulla nemo corporis commodi quos animi temporibus?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, laborum.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aspernatur impedit cupiditate perferendis nemo facere velit consequatur commodi, incidunt eveniet rerum doloribus tenetur magnam repellat voluptatibus hic provident vero earum omnis magni in. Repellat ratione praesentium sunt, nihil harum ipsum aut aspernatur similique, a molestias explicabo quam. Totam sapiente sed provident neque itaque veritatis nulla quaerat corrupti obcaecati quos? Possimus accusamus, iste illo et, repellat quod quia facere, voluptatibus totam vero laborum nostrum consequatur? Hic debitis natus officiis. Optio esse dolor nobis neque veritatis, repellendus reiciendis porro tenetur? Consequuntur eveniet explicabo est autem nulla nemo corporis commodi quos animi temporibus?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, laborum.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aspernatur impedit cupiditate perferendis nemo facere velit consequatur commodi, incidunt eveniet rerum doloribus tenetur magnam repellat voluptatibus hic provident vero earum omnis magni in. Repellat ratione praesentium sunt, nihil harum ipsum aut aspernatur similique, a molestias explicabo quam. Totam sapiente sed provident neque itaque veritatis nulla quaerat corrupti obcaecati quos? Possimus accusamus, iste illo et, repellat quod quia facere, voluptatibus totam vero laborum nostrum consequatur? Hic debitis natus officiis. Optio esse dolor nobis neque veritatis, repellendus reiciendis porro tenetur? Consequuntur eveniet explicabo est autem nulla nemo corporis commodi quos animi temporibus?',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, laborum.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aspernatur impedit cupiditate perferendis nemo facere velit consequatur commodi, incidunt eveniet rerum doloribus tenetur magnam repellat voluptatibus hic provident vero earum omnis magni in. Repellat ratione praesentium sunt, nihil harum ipsum aut aspernatur similique, a molestias explicabo quam. Totam sapiente sed provident neque itaque veritatis nulla quaerat corrupti obcaecati quos? Possimus accusamus, iste illo et, repellat quod quia facere, voluptatibus totam vero laborum nostrum consequatur? Hic debitis natus officiis. Optio esse dolor nobis neque veritatis, repellendus reiciendis porro tenetur? Consequuntur eveniet explicabo est autem nulla nemo corporis commodi quos animi temporibus?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
     }
 }
--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.main')
 @section('container')
 <h1 class="mb-5">{{ $post->title }}</h1>
-<p>By. Muhamad Rizky Fauzan in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+<p>By. <a href="# " " class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 {!! $post->body !!}
 
-<a href="/blog">Back to Posts</a>
+<a href="/blog" class="d-block">Back to Posts</a>
 @endsection
