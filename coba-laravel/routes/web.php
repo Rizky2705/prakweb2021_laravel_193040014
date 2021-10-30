f2fe4169dfbcf2b1ad91a35456ea541ee38fc0f9
MRizkyF27 <63577711+Rizky2705@users.noreply.github.com>
2021-10-30 16:32:04 +0700

    Menyelesaikan video Login & registration

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
-
+// use App\Models\Post;
+// use App\Models\User;
 use App\Models\Category;
-use App\Models\User;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,7 +49,6 @@ Route::get('/categories', function () {
     ]);
 });
 
-
 // Route::get('/categories/{category:slug}', function (Category $category) {
 //     return view('posts', [
 //         'title' => "Post By Category : $category->name",
@@ -64,3 +65,10 @@ Route::get('/categories', function () {
 //         'posts' => $author->posts->load('category', 'author')
 //     ]);
 // });
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
+
+
+
