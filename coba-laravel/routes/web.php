f6a83fd45876fd198f4747d08d472ecbc6311001
MRizkyF27 <63577711+Rizky2705@users.noreply.github.com>
2021-10-30 22:45:16 +0700

    Menyelesaikan video User Login & Middleware

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -2,6 +2,8 @@
 
 // use App\Models\Post;
 // use App\Models\User;
+
+use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 
 use Illuminate\Support\Facades\Route;
@@ -66,10 +68,13 @@ Route::get('/categories', function () {
 //     ]);
 // });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 
 
